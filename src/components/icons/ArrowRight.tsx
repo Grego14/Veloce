a814bf60aac9740d1fbd0e2a8726ccf6c5be2ade
Gregorio Piñero <gre208981@gmail.com>
2026-09-09 23:33:05 -0400

    add used inline icons

---
+++ src/components/icons/ArrowRight.tsx
@@ -0,0 +1,20 @@
+export default function ArrowRight({ className = 'w-5 h-5', strokeWidth = 2 }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={strokeWidth}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      width="24"
+      height="24"
+      className={className}
+      aria-hidden="true"
+    >
+      <path d="M5 12h14" />
+      <path d="m12 5 7 7-7 7" />
+    </svg>
+  )
+}
